7b9fd609404a351b5f01e841a94b5322823cd834
sato gaito <gaito4570@gmail.com>
2020-01-09 20:28:05 +0900

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,7 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3cd291ac93736cd62bbbba07ff9b5d2b61c3a75e2f20ee2c4af12039e3fb139530ca23bbd0f1777a39f31586f8f855b53ba436d66b8232d7b68f17876d5d3e14'
+
+
